8049a8c93b8564e9a31d48de14328556c0e67258
bestiadivina147 <119857701+bestiadivina147@users.noreply.github.com>
2024-01-09 12:55:19 +0100

    refactor
    chances in "def change"

--- src/yatzy.py
+++ src/yatzy.py
@@ -1,14 +1,8 @@
 class Yatzy:
-
+    # Cambie para que se pueda usar siendo diferente de 5 dados
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*args):
+        return sum(args)
 
     @staticmethod
     def yatzy(dice):
